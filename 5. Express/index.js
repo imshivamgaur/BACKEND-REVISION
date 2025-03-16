12d708565be91e51905d6e497af341bd39661e4a
iamshivamgaur <shivam24august@gmail.com>
2025-03-16 17:07:28 +0530

    Adding Express

---
+++ 5. Express/index.js
@@ -0,0 +1,61 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+
+let movieData = [];
+let nextId = 1;
+
+// Add movie
+app.post("/add-movie", (req, res) => {
+  let { name } = req.body;
+  let newMovie = { id: nextId++, name: name };
+  movieData.push(newMovie);
+  res.status(200).json(newMovie);
+});
+
+// Get all movies
+app.get("/movies", (req, res) => {
+  res.status(200).send(movieData);
+});
+
+// Get movie with id
+app.get("/movies/:id", (req, res) => {
+  let { id } = req.params;
+  const data = movieData.find((movie) => parseInt(id) === movie.id);
+  // console.log(data);
+  if (!data) {
+    return res.status(404).send(`Movie Not Found`);
+  }
+  res.status(200).json(data);
+});
+
+// Update movie
+app.put("/movies/:id", (req, res) => {
+  let { id } = req.params;
+  const data = movieData.find((movie) => parseInt(id) === movie.id);
+
+  if (!data) {
+    return res.status(404).send("Movie Not Found");
+  }
+  const { name } = req.body;
+  data.name = name;
+  res.status(200).send(data);
+});
+
+// Delete Route
+app.delete("/movies/:id", (req, res) => {
+  let { id } = req.params;
+  const index = movieData.findIndex((movie) => parseInt(id) === movie.id);
+  console.log(index);
+  if (index === -1) {
+    return res.status(404).send("Movie Not Found");
+  }
+  movieData.splice(index, 1);
+  return res.status(200).send("Deleted!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
